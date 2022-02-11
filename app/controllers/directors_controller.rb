dd352bfb2d1854418426756c2923f897f89fd89b
divyakesharwani <dkesharw@chicagobooth.edu>
2022-02-11 03:45:59 +0000

    first directors index tab pass

---
+++ app/controllers/directors_controller.rb
@@ -0,0 +1,11 @@
+class DirectorsController < ApplicationController
+
+  def index
+    @list_of_directors = Director.all 
+    
+    render({:template => "director_templates/index.html.erb"})
+  end
+
+
+end
+
